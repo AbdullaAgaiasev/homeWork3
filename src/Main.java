20dab2fdad7757f29f005c523930182fe8740c1d
mac <abdullaagajsaev@gmail.com>
2022-10-09 16:34:27 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main (String[] args) {
+        int[] numbers_1 = {4 / 3, -14 / 9, 5 / 2, -7 / 6, 10 / 8};
+        System.out.println(Arrays.toString(numbers_1));
+        {
+
+            int[] numbers = {8, -2, -4, 2, 3, 6};
+            System.out.println(" ************** ");
+            System.out.println(numbers[3] + numbers[4] + numbers[5] / 3);
+
+            public static void=(int[] array;) {
+            for (int i = 1; i < array.length;i++) {
+                  int current = array [i];
+                  int j = i;
+                  while (j > 0 && array[j - 1] > current){
+                      array [j] = array[j-1];
+                      j--;
+                  }
+                  array[j] = current;
+              }
+
+        }
+        }
+    }
+}
+
+
+
+
